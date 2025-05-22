4d10a99f156665ad9516162879d785dc77673c98
hannasahlberg <hannassahlberg@live.se>
2025-05-22 10:49:54 +0200

    små ändringar

--- vite-react-uppgift/src/App.jsx
+++ vite-react-uppgift/src/App.jsx
@@ -1,10 +1,9 @@
-import { useState } from 'react'
+import React from 'react'
 import './App.css'
 import Navbar from './components/navbar';
 import Sidebar from './components/sidebar';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
